af4dcfb5cd92023ef1cdcb2668b8cfd5241abc2e
Daemon <mfadlanf08@gmail.com>
2025-09-29 11:45:15 +0700

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
