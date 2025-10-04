c2e3626122f433cad3586528ff2145c7e7d7c0f3
Daemonic10 <mfadlanf08@gmail.com>
2025-10-04 08:53:39 +0700

    test pc

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,23 @@
-export default function Footer(){
-    return <p>Footer ANJINGGGGG</p>
+
+
+function footer() {
+  return (
+    <div>
+        <div className="atas">
+            <h1>Semangat Menjelajahi Teknologi? Kami juga. Ayo Mulai Bersama!</h1>
+            <div className=""><button>Chat on WhatsApp</button></div>
+
+       </div>
+       <div className="bawah">
+        <div className="kanan">
+
+        </div>
+        <div className="kiri">
+        
+        </div>
+       </div>
+    </div>
+  )
 }
+
+export default footer
