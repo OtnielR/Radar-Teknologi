985e5d9f6704c8981f8e9b8222decde32be6152b
Daemonic10 <mfadlanf08@gmail.com>
2025-10-04 09:34:53 +0700

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Footer from "./components/footer";
+import Herosection from "./components/Herosection"
+import About from "./components/About"
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Herosection/>
+      <About/>
       <Footer></Footer>
     </>
   );
