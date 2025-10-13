0b8732c63cd772cb6dd02caa307772568f451348
Daemon <mfadlanf08@gmail.com>
2025-10-13 10:00:06 +0700

    Your message about the changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,3 +1,28 @@
-export default function Footer(){
-    return <p>Footer ANJINGGGGG</p>
+export default function Footer() {
+  return (
+    <>
+      <div>
+        <div className="max-w-5xl mx-auto mt-8 px-4">
+          <div className="bg-white shadow-md rounded-xl flex flex-col md:flex-row items-center justify-between p-4">
+            <p className="text-center md:text-left font-medium">
+              Semangat Menjelajahi Teknologi?{" "}
+              <span className="font-bold">Kami juga. Ayo Mulai Bersama!</span>
+            </p>
+            <a
+              href="https://wa.me/6281234567890"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-3 md:mt-0 bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700"
+            >
+              Chat on WhatsApp
+            </a>
+          </div>
+        </div>
+        <div>
+            
+            
+        </div>
+      </div>
+    </>
+  );
 }
